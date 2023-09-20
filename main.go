308bcbf06c927ae56f5e654ebc448ec85f198fe5
anyaguuu <ag156@rice.edu>
2023-09-20 17:48:13 -0500

    wg methods

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"sync"
 
 	"github.com/anyaguuu/concurrent_lists/structs"
 )
+
+func wgFind[K cmp.Ordered, V any](wg *sync.WaitGroup, lst structs.List[K, V], key K) (V, bool) {
+	defer wg.Done()
+	return lst.Find(key)
+}
+func wgInsert[K cmp.Ordered, V any](wg *sync.WaitGroup, lst structs.List[K, V], key K, val V) bool {
+	defer wg.Done()
+	return lst.Insert(key, val)
+}
+func wgRemove[K cmp.Ordered, V any](wg *sync.WaitGroup, lst structs.List[K, V], key K) (V, bool) {
+	defer wg.Done()
+	return lst.Remove(key)
+}
 
 func main() {
 	lst := structs.New[int, int](0, 10)
