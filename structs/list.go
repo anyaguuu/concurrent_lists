fac80b73b765d3a82186b23f608c493a30e0d6f1
anyaguuu <ag156@rice.edu>
2023-09-20 14:59:39 -0500

    reorg + new list

--- structs/list.go
+++ structs/list.go
@@ -2,10 +2,35 @@ package structs
 
 import (
 	"cmp"
-
-	"github.com/anyaguuu/concurrent_lists/node"
+	"sync"
+	"sync/atomic"
 )
 
+type node[K cmp.Ordered, V any] struct {
+	sync.Mutex
+	key    K
+	item   V
+	marked atomic.Bool // zero value is false
+	next   atomic.Pointer[node[K, V]]
+}
+
 type List[K cmp.Ordered, V any] struct {
-	head *node.Node[K, V]
+	head *node[K, V]
+}
+
+// type ConcurrentList[K cmp.Ordered, V any] interface {
+// 	Find(K, V, bool)    // does not remove node, else same as remove
+// 	Insert(K, V) bool   // returns true if inserted, else false (already there)
+// 	Remove(K) (V, bool) // returns val, ok (false if no node with key)
+// }
+
+func New[K cmp.Ordered, V any](minKey K, maxKey K) List[K, V] {
+	tail := new(node[K, V])
+	tail.key = maxKey
+	head := new(node[K, V])
+	head.key = minKey
+	head.next.Store(tail)
+
+	list := List[K, V]{head: head}
+	return list
 }
