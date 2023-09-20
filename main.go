af64525ed1b3ed46f3d3fd709d86c2b1738691b0
anyaguuu <ag156@rice.edu>
2023-09-20 16:00:18 -0500

    simple test

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/anyaguuu/concurrent_lists/structs"
+)
+
 func main() {
-	lst := concurrentlist.New()
+	lst := structs.New[int, int](0, 10)
+	lst.Insert(1, 1)
+	lst.Insert(2, 2)
+	lst.Insert(3, 3)
+	lst.Insert(10, 10)
+	lst.Insert(4, 4)
+
+	find := 3
+	_, ok := lst.Find(find)
+	if ok {
+		fmt.Println("found", find)
+	} else {
+		fmt.Println("could not find ")
+	}
+
 }
